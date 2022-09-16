a0edf2037639bcb5415e00f3030d72577e48dc7b
juroga <juanjrgast@gmail.com>
2022-09-16 17:19:29 +0200

    Update the comment of the getComputerChoice

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let ComputerChoice = 0;
-
+//Create a function that randomly pick a number between 0-2, then we will
+//assign virtually a value to each number: 0 - Rock, 1 - Paper, 2 - Scissors
+//So we can compare them later with the userChoice
 function getComputerChoice() {
     ComputerChoice = Math.floor(Math.random()*3);
     return ComputerChoice;
 }
+
+
 
