8dbcdeeb1bbc069c59607521891b3a92ea9e0866
juroga <juanjrgast@gmail.com>
2022-09-16 17:50:15 +0200

    create a userChoice function and update the computerChoice to return strings instead of numbers

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-let ComputerChoice = 0;
+
 //Create a function that randomly pick a number between 0-2, then we will
 //assign virtually a value to each number: 0 - Rock, 1 - Paper, 2 - Scissors
 //So we can compare them later with the userChoice
 function getComputerChoice() {
+    let ComputerChoice = 0;
     ComputerChoice = Math.floor(Math.random()*3);
+    switch (ComputerChoice) {
+        case 0:
+            return "ROCK";
+           break;
+        case 1:
+            return "PAPER";
+            break;
+        case 2:
+            return "SCISSORS";
+            break;
+    }
     return ComputerChoice;
 }
 
-
+//Create a function that pop up a prompt asking for the choice. Once the user writes it down,
+//it converts it to upper cases and check if the spelling is right. If is not right, it send 
+//you a message warning you (still pending to start from the beginning changing the message)
+//if the spelling is right, it returns the user choice in capital letters
+function getUserChoice() {
+    let userChoice = prompt("Write down your choice", "Rock, Paper or Scissors");
+    userChoice = userChoice.toUpperCase();
+    if (userChoice !== "ROCK" || "PAPER" || "SCISSORS") {
+        let wrongUserChoiceMessage = "Please, check your spelling";
+        return wrongUserChoiceMessage;
+    }
+    return userChoice;
+}
 
